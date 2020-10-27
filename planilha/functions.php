367d16f68a32798408ee06e0d8378f1543c4e42e
Lucas Medeiros <lucas.medeiros@rockcontent.com>
2020-10-27 11:15:43 -0300

    Fix favicon path

--- planilha/functions.php
+++ planilha/functions.php
@@ -21,7 +21,7 @@ function template_header($title)
         <head>
             <meta charset="utf-8">
             <title>$title</title>
-            <link rel="icon" type="image/png" href="./favicon.ico"/>
+            <link rel="icon" type="image/png" href="https://lucascmedeiros.com.br/bgbh/favicon.ico"/>
             <link href="dist/assets/styles/style.css" rel="stylesheet" type="text/css">
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
