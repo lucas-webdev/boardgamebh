ca2faef77ba792b5b8620e7ff1e4896c21fa16bf
Lucas Medeiros <lucascmedeiros.dev@gmail.com>
2024-12-11 18:03:12 +0000

    fix scss import and deploy multiple html

---
+++ src/pages/quem-somos/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const QuemSomos: FC = () => (
+    <div>QUEM SOMOS</div>
+)
+
+export default QuemSomos
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Define o alias '@' para 'src'
+    },
   },
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/variables" as *;`, // Caminho absoluto correto
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',  // Diretório de saída onde os arquivos estáticos serão gerados
+    rollupOptions: {
+      input: {
+        main: 'index.html',  // Arquivo HTML principal
+        'quem-somos': 'src/pages/quem-somos/index.tsx',  // Rota adicional
+        'mathtrade-o-que-e': 'src/pages/mathtrade/MathTrade.tsx',
+        'mathtrade-regras-gerais': 'src/pages/mathtrade/RegrasGeraisMT.tsx',
+        // Adicione outras rotas conforme necessário
+      },
+    },
+  },
+});
+
