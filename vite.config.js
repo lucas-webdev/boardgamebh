55f098b5cd585c6a24b22a69376f6e0c1f5762b2
Lucas Medeiros <lucascmedeiros.dev@gmail.com>
2025-01-11 10:29:46 -0300

    set base 2025

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/",
+  base: "/2025",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
