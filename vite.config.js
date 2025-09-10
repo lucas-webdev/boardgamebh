8a02106d8d8ae30ca2a3992da959877c68eb3b9f
Lucas Medeiros <lucas@refersion.com>
2025-09-10 11:47:26 -0300

    bgbh em reformulacao

--- vite.config.js
+++ vite.config.js
@@ -2,46 +2,46 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/2025",
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
-  },
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        "sobre/": resolve(__dirname, "sobre/index.html"),
-        "bgbh-masters/": resolve(__dirname, "bgbh-masters/index.html"),
-        "em-breve": resolve(__dirname, "em-breve.html"),
-        "mathtrade-bh/o-que-e": resolve(__dirname, "mathtrade-bh/o-que-e.html"),
-        "mathtrade-bh/faq/": resolve(__dirname, "mathtrade-bh/faq.html"),
-        "mathtrade-bh/edicao-novembro-2024": resolve(
-          __dirname,
-          "mathtrade-bh/edicao-novembro-2024.html"
-        ),
-        "mathtrade-bh/edicao-novatos": resolve(
-          __dirname,
-          "mathtrade-bh/edicao-novatos.html"
-        ),
-        "mathtrade-bh/regras-gerais": resolve(
-          __dirname,
-          "mathtrade-bh/regras-gerais.html"
-        ),
-        "mathtrade-bh/edicao-setembro-2025": resolve(
-          __dirname,
-          "mathtrade-bh/edicao-setembro-2025.html"
-        ),
-      },
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/styles/_variables.scss" as *;`,
-      },
-    },
-  },
+	base: "/",
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "src"),
+		},
+	},
+	build: {
+		outDir: "dist",
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				"sobre/": resolve(__dirname, "sobre/index.html"),
+				"bgbh-masters/": resolve(__dirname, "bgbh-masters/index.html"),
+				"em-breve": resolve(__dirname, "em-breve.html"),
+				"mathtrade-bh/o-que-e": resolve(__dirname, "mathtrade-bh/o-que-e.html"),
+				"mathtrade-bh/faq/": resolve(__dirname, "mathtrade-bh/faq.html"),
+				"mathtrade-bh/edicao-novembro-2024": resolve(
+					__dirname,
+					"mathtrade-bh/edicao-novembro-2024.html"
+				),
+				"mathtrade-bh/edicao-novatos": resolve(
+					__dirname,
+					"mathtrade-bh/edicao-novatos.html"
+				),
+				"mathtrade-bh/regras-gerais": resolve(
+					__dirname,
+					"mathtrade-bh/regras-gerais.html"
+				),
+				"mathtrade-bh/edicao-setembro-2025": resolve(
+					__dirname,
+					"mathtrade-bh/edicao-setembro-2025.html"
+				),
+			},
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@use "@/styles/_variables.scss" as *;`,
+			},
+		},
+	},
 });
