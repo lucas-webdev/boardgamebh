9adfac946e327d3fc418d3dc251ed69c3b659d4d
Lucas Medeiros <lucascmedeiros.dev@gmail.com>
2026-04-03 14:25:52 -0300

    fix pwa

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,6 @@
 const CACHE_NAME = 'bgbh-v1';
-const PRECACHE_URLS = [
-  '/',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
-];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
-  );
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
